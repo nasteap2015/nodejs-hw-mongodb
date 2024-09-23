8b83607c754f3d027893f73ca72834dd923da211
nasteap2015 <149483716+nasteap2015@users.noreply.github.com>
2024-09-23 12:24:08 +0200

    errorHandler corrected

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 export const errorHandler = (err, req, res, next) => {
-  res.status(500).json({
+  res.status(err.status).json({
     message: 'Something went wrong',
     error: err.message,
     data: err,
