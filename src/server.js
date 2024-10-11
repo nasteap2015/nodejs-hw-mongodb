b9bc6460b380bcf260fc99fb23b562b38a557a43
nasteap2015 <149483716+nasteap2015@users.noreply.github.com>
2024-10-11 14:39:44 +0200

    swagger added

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -15,6 +16,7 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
